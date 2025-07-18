69fd4318cebfca48a08136a42e9f61c630e401f9
AminMarayed <aminmarayed4@gmail.com>
2025-07-18 11:37:34 +0330

    Add server configuration to Vite for HMR and file system watching

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // Force HMR connection to use WSS
+    hmr: {
+      overlay: true,
+    },
+    // Enable file system watching
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
 });
