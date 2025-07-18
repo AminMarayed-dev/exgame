c10d0f1efbb975592f1d332d784d013c2560ab4f
AminMarayed <aminmarayed4@gmail.com>
2025-07-18 17:16:54 +0330

    Add ProgressBar and ScrollToTop components to MainLayout for enhanced navigation and user experience

--- src/layout/main/MainLayout.tsx
+++ src/layout/main/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"; // This renders the nested route content
 import Header from "./Header";
 import Footer from "./Footer";
+import { ProgressBar, ScrollToTop } from "../../components/common";
 
 function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ProgressBar />
       <Header />
       <main className="flex-grow w-full pt-16 md:pt-20">
         {/* <div className="container mx-auto"> */}
@@ -12,6 +14,7 @@ function MainLayout() {
         <Outlet />
         {/* </div> */}
       </main>
+      <ScrollToTop />
       <Footer />
     </div>
   );
