cdd54e4e5fcc1413fcb92cc422c95fd0e4781400
AminMarayed <aminmarayed4@gmail.com>
2025-07-18 16:39:02 +0330

    Integrate ToastProvider into main application and comment out Header and Footer components in Layout for future implementation

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,15 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Loading from "../components/common/Loading";
-import Header from "./main/Header";
-import Footer from "./main/Footer";
+
 
 const Layout = () => {
   return (
     <div className="min-h-screen bg-background text-foreground dark">
       <Suspense fallback={<Loading />}>
-        <Header />
+        {/* <Header /> */}
         <Outlet />
-        <Footer />
+        {/* <Footer /> */}
       </Suspense>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { HeroUIProvider } from "@heroui/react";
+import {ToastProvider} from "@heroui/toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HeroUIProvider>
+    <HeroUIProvider>  
+      <ToastProvider />
       <RouterProvider router={router} />
     </HeroUIProvider>
   </StrictMode>
